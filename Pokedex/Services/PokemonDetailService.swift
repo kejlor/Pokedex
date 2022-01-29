f1b9f025963a818a7189932258c99276346d422c
kejlor <woj450@gmail.com>
2022-01-29 20:25:19 +0100

    Temporary solution with getting images

--- Pokedex/Services/PokemonDetailService.swift
+++ Pokedex/Services/PokemonDetailService.swift
@@ -14,13 +14,13 @@ class PokemonDetailService {
     @Published var image: UIImage? = nil
     
     private var imageSubscription: AnyCancellable?
-    private var linkSubscription: AnyCancellable?
-    private var spriteURL: PokemonSpritesModel? = nil
+//    private var linkSubscription: AnyCancellable?
+//    private var spriteURL: PokemonSpritesModel? = nil
     private let pokedexEntry: PokedexModel
     
     init(pokedexEntry: PokedexModel) {
         self.pokedexEntry = pokedexEntry
-        getSpriteURL()
+//        getSpriteURL()
         downloadSpriteImage()
     }
     
@@ -38,23 +38,25 @@ class PokemonDetailService {
 //        }.resume()
 //    }
     
-    private func getSpriteURL() {
-        guard let url = URL(string: pokedexEntry.url) else { return }
+//    private func getSpriteURL() {
+//        guard let url = URL(string: pokedexEntry.url) else { return }
 //        print(url)
-        linkSubscription = NetworkingManager.download(url: url)
-            .decode(type: PokemonSpritesModel.self, decoder: JSONDecoder())
-            .receive(on: DispatchQueue.main)
-            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedSpriteURL) in
+//        linkSubscription = NetworkingManager.download(url: url)
+//            .decode(type: PokemonSpritesModel.self, decoder: JSONDecoder())
+//            .receive(on: DispatchQueue.main)
+//            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedSpriteURL) in
 //                guard let self = self, let downloadedSprite = returnedSpriteURL.frontDefault else { return }
-                self?.spriteURL = returnedSpriteURL
-                self?.linkSubscription?.cancel()
-            })
+//                self?.spriteURL = returnedSpriteURL
+//                self?.linkSubscription?.cancel()
+//            })
 //        print(self.spriteURL?.frontDefault!)
-    }
+//    }
     
     private func downloadSpriteImage() {
-        guard let url = URL(string: spriteURL?.frontDefault ?? "") else { return }
-        print("getting image")
+        let string = pokedexEntry.url
+        let stringArray = string.suffix(4).components(separatedBy: CharacterSet.decimalDigits.inverted).joined()
+        print(stringArray)
+        guard let url = URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/\(stringArray).png") else { return }
         imageSubscription = NetworkingManager.download(url: url)
             .tryMap({ (data) -> UIImage? in
                 return UIImage(data: data)
